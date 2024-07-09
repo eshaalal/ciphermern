9f9307704352ce406cc6af9f4f70c08f3ac9dd9e
eshaalal <eshalal888@gmail.com>
2024-07-09 19:48:16 +0530

    changes

--- jwt/index.js
+++ jwt/index.js
@@ -1,26 +1,19 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
 const jwtPassword = "123456";
 const app = express();
 app.use(express.json());
 
-const ALL_USERS = [
-    {
-        username: "eshalal@gmail.com",
-        password: "123",
-        name: "esha lal"
-    },
-    {
-        username: "lana@gmail.com",
-        password: "1234",
-        name: "lana del rey"
-    },
-    {
-        username: "chotu@gmail.com",
-        password: "123",
-        name: "chotu lal"
-    }
-];
+mongoose.connect(
+    "mongodb+srv://eshalal9693:esha2127E@cluster0.3mrzxzx.mongodb.net/user_app",
+  );
+
+const User = mongoose.model("Users", {
+    name: String,
+    email: String,
+    password: String,
+  }); 
 
 function userExists(username, password) {
     let userExists=false;
@@ -60,6 +53,24 @@ app.get("/users", function (req, res) {
         });
     }
 });
+app.post("/signup",async function(req,res){
+    const name = req.body.name;
+    const username = req.body.username;
+    const password = req.body.password;
+    const existingUser= await User.findOne({email:username});
+    if(existingUser){
+        return res.status(400).send("username already exist");
+    }
+    const user=new User({
+        name:name,
+        email:username,
+        password:password
+    })
+    user.save();
+    res.json({
+        "msg":"user created succesfully"
+    });
+})
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
