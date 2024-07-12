4c76b2183cd320231d2d16ad412c6fd3f2e5b316
eshaalal <eshalal888@gmail.com>
2024-07-12 12:07:28 +0530

    changes

--- todo_react/src/App.jsx
+++ todo_react/src/App.jsx
@@ -4,21 +4,43 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0);
-  function onClickHandler(){
-    setCount(count+1);
+  const [todos, setTodos] = useState([{
+    title:"Go to gym",
+    description:"Go to gym from 7-9",
+    completed:false
+  },{
+    title:"Study DSA",
+    description:"Study DSA from 9-10",
+    completed:true
+  }
+  ]);
+
+  function addTodo(){
+    setTodos([...todos,{
+      title:"new todo",
+      description:"description of new todo"
+    }])
   }
 
   return (
     
       <div>
-        <button onClick={onClickHandler}>count {count}</button>
-       
+        <button onClick={addTodo}>Add a random todo</button>
+       {todos.map(function(todo){
+        return <Todo title={todo.title} description={todo.description}/>
+       })}
         
       </div>
       
     
   )
+}
+//component
+function Todo(props){
+  return <div>
+    <h1>{props.title}</h1>
+    <h1>{props.description}</h1>
+  </div>
 }
 
 export default App
